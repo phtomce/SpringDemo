da8d5ed406f85df9c88e8119e8ef30531bcfddcd
Tomas <phtomce@gmail.com>
2023-04-04 18:57:01 +0300

    add customer repository

---
+++ src/main/java/lt/tomas/SpringDemo/repository/customers/CustomerRepository.java
@@ -0,0 +1,6 @@
+package lt.tomas.SpringDemo.repository.customers;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer,Integer> {
+}
