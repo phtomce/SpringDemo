7675fc1ca18f26e802ddeca45a4a8568d6f19b6d
Tomas <phtomce@gmail.com>
2023-04-04 20:44:44 +0300

    add method get all customers

--- src/main/java/lt/tomas/SpringDemo/controller/CustomerController.java
+++ src/main/java/lt/tomas/SpringDemo/controller/CustomerController.java
@@ -1,17 +1,31 @@
 package lt.tomas.SpringDemo.controller;
 
+import lt.tomas.SpringDemo.repository.customers.Customer;
+import lt.tomas.SpringDemo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller // localhost:8080 -> 127.0.0.1:8080
 @RequestMapping(path = "/customer") // localhost:8080/customer
-public class CustomerController  {
+public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
 
     // http://localhost:8080/customer/test
     @GetMapping(path = "/test")
-    public @ResponseBody String getTestPage(){
+    public @ResponseBody String getTestPage() {
         return "This is some text from SPRING";
+    }
+
+    //http://localhost:8080/customer/all
+    @GetMapping(path = "/all")
+    public @ResponseBody List<Customer> getAllCustomers() {
+        return customerService.getallcustomers();
     }
 }
